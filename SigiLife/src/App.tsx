35fa5f0440e8e3200eca757cf8a25fda07ca3e3f
HopeyClarkey <thehopeclarke@gmail.com>
2026-03-19 23:03:16 -0500

    update landing page

--- SigiLife/src/App.tsx
+++ SigiLife/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import SigiLifeLogo from '../public/SigiLife Logo.png'
 import './App.css'
 
 function App() {
@@ -11,9 +9,8 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src={SigiLifeLogo} className="base" width="100%" height="100%" alt="Sigil-Life-Logo" />
+
         </div>
         <div>
           <h1>Welcome to SigiLife</h1>
