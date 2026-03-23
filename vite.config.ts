8157d372d122e637a0b914d5134a568ae5c7e5e9
HopeyClarkey <thehopeclarke@gmail.com>
2026-03-23 17:09:32 -0500

    remove emotion, add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,5 @@ export default defineConfig({
     host: true,
     allowedHosts: ['ec2-18-223-34-170.us-east-2.compute.amazonaws.com'],
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
 });
