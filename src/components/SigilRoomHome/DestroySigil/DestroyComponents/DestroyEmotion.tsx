87274b2d82f6afa5f7a2b9c2a9ede68cb6b2f1cc
HopeyClarkey <thehopeclarke@gmail.com>
2026-04-04 20:08:27 -0500

    update for sigilFriends

--- src/components/SigilRoomHome/DestroySigil/DestroyComponents/DestroyEmotion.tsx
+++ src/components/SigilRoomHome/DestroySigil/DestroyComponents/DestroyEmotion.tsx
@@ -4,15 +4,7 @@ export default function DestroyEmotion() {
 
   return (
     <div>
-      <div className="flameshow">
-        <img src="src/assets/Flame/1.png" alt="Slide 1" />
-        <img src="src/assets/Flame/2.png" alt="Slide 2" />
-        <img src="src/assets/Flame/3.png" alt="Slide 3" />
-        <img src="src/assets/Flame/4.png" alt="Slide 4" />
-        <img src="src/assets/Flame/5.png" alt="Slide 5" />
-        <img src="src/assets/Flame/6.png" alt="Slide 6" />
-        <img src="src/assets/Flame/7.png" alt="Slide 7" />
-      </div>
+
     </div>
   )
 };
